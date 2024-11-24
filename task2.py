7f143677c8a18a38bea19bc78b425c516ab43405
romam <romamar2004@gmail.com>
2024-11-24 15:32:23 +0300

    color fix

--- task2.py
+++ task2.py
@@ -14,7 +14,7 @@ resize_factor = 0.75
 resized_image = cv2.resize(rotated_image, (int(width * resize_factor), int(height * resize_factor)))
 
 new_height, new_width = resized_image.shape[:2]
-line_color = (255, 0, 0)
+line_color = (0, 0, 255)
 line_thickness = 2
 cv2.line(resized_image, (new_width // 2, 0), (new_width // 2, new_height), line_color, line_thickness)
 
@@ -22,7 +22,7 @@ display_text = "%#?!!a1T"
 text_position = (150, new_height - 10)
 font_type = cv2.FONT_HERSHEY_SIMPLEX
 font_scale = 1
-text_color = (255, 0, 0)  # Цвет текста (синий в формате BGR)
+text_color = (0, 0, 255)
 text_thickness = 2
 cv2.putText(resized_image, display_text, text_position, font_type, font_scale, text_color, text_thickness)
 
